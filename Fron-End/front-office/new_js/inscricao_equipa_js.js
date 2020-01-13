b292913b0757584d8d1e880e7b9d218e2514ae90
Alvaro Ferreira <alvarobahrain@gmail.com>
2020-01-13 18:19:27 +0000

    vsdvsdv

--- Fron-End/front-office/new_js/inscricao_equipa_js.js
+++
@@ -1,5 +0,0 @@
-var table = document.getElementById('tabela-equipa');
-var row = table.insertRow(1);
-row.innerHTML = "<td>"+nome+"</td> <td>"+presidio+"</td><td>"+cidade+"</td><td>"+status+"</td>";
-
-
